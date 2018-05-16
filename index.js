8649e3080e93c08f02af7e48b522de516688a547
王松_Striker <song@secbox.cn>
2018-05-17 00:42:29 +0800

    :sparkles: fished get local video duration.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const electron = require("electron")
-const { app, BrowserWindow } = electron
+const electron = require("electron");
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+
+let mainWindow;
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({})
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
-})
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+       mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
+});
