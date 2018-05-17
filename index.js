efe9b779b9ee34388b84f8cda21b00d946ee2f9e
王松_Striker <song@secbox.cn>
2018-05-17 12:25:59 +0800

    :sparkles: fished todo list.

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
 const electron = require("electron");
-const { app, BrowserWindow, ipcMain } = electron;
-const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on("ready", () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.on('closed', () => app.quit());
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
 
-ipcMain.on('video:submit', (event, path) => {
-    ffmpeg.ffprobe(path, (err, metadata) => {
-       mainWindow.webContents.send('video:metadata', metadata.format.duration);
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo',
     });
-});
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on('closed', () => addWindow = null);
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+})
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Todos',
+                click() {
+                    mainWindow.webContents.send('todo:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
+
+if (process.platform === 'darwin'){
+    menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu:[
+            {
+                role: 'reload'
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
